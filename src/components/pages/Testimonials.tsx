f13f6ae1a9a09c3403af282385d34ef991499355
Edwin Estrella <edwinestrella67@gmail.com>
2025-11-03 21:10:24 -0400

    Centralizado de letras

--- src/components/pages/Testimonials.tsx
+++ src/components/pages/Testimonials.tsx
@@ -80,7 +80,7 @@ const Testimonials = () => {
             <div className="border py-1 px-4 rounded-lg">Testimonios</div>
           </div>
 
-          <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold tracking-tighter mt-5">
+          <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold tracking-tighter mt-5 text-center">
             Lo que dicen nuestros usuarios
           </h2>
           <p className="text-center mt-5 opacity-75">
